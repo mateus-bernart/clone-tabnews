a99298d09f63533f9d8b5c8ef8610e68718d5b21
Mateus Bernart <80543817+mateus-bernart@users.noreply.github.com>
2024-01-06 14:52:39 +0000

    adiciona scripts  `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # clone-tabnews
+
 Projeto para desenvolver algumas habilidades com o curso.dev
 Clone do site https://www.tabnews.com.br/
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
-function Home(){
-    return <h1>Oi jo meu amor jaja to indo ai te ver ❤️</h1>
+function Home() {
+  return <h1>Oi jo meu amor jaja to indo ai te ver ❤️</h1>;
+}
+
+function teste() {
+  console.log("identação errada");
 }
 
 export default Home;
