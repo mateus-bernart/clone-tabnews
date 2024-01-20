4648c74ff40030ccf9cf765ee4f60f1c8e9dd261
Mateus Bernart <80543817+mateus-bernart@users.noreply.github.com>
2024-01-20 02:57:43 +0000

    add `database.js` and `.env` files

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,5 +1,9 @@
-function status(request, response) {
-  response.status(200).json({ chave: "sao acima da média" });
+import database from "../../../../infra/database.js";
+
+async function status(request, response) {
+  const result = await database.query("SELECT 1 + 1 AS sum;");
+  console.log(result.rows);
+  response.status(200).json({ chave: "Olá mundo! UTF-8!" });
 }
 
 export default status;
