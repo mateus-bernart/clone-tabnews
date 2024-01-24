987d32bb74bd54353552a1782e0251db551b494e
Mateus Bernart <80543817+mateus-bernart@users.noreply.github.com>
2024-01-24 00:55:32 +0000

    finish `/api/v1/status endpoint`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,32 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 AS sum;");
-  console.log(result.rows);
-  response.status(200).json({ chave: "Olá mundo! UTF-8!" });
+  const updatedAt = new Date().toISOString(); //config to ISO pattern
+
+  const databaseVersionResult = await database.query("SHOW server_version;");
+  const databaseVersionValue = databaseVersionResult.rows[0].server_version;
+
+  const maxConnectionsResult = await database.query("SHOW max_connections;");
+  const maxConnectionsValue = maxConnectionsResult.rows[0].max_connections;
+
+  const databaseName = process.env.POSTGRES_DB;
+  const openedConnectionsResult = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname= $1;",
+    values: [databaseName],
+  });
+  const openedConnectionsValue = await openedConnectionsResult.rows[0].count;
+  console.log(openedConnectionsValue);
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: databaseVersionValue,
+        max_connections: parseInt(maxConnectionsValue),
+        opened_connections: openedConnectionsValue,
+      },
+    },
+  });
 }
 
 export default status;
