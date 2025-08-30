52dd24440fdbaac67288cf4681b26d894b744dff
Mateus Davi Bernart <mateusbernart14@gmail.com>
2025-08-30 17:48:36 -0300

    feat: adds model migrator

---
+++ models/migrator.js
@@ -0,0 +1,64 @@
+import database from "infra/database";
+import { ServiceError } from "infra/errors";
+import migrationRunner from "node-pg-migrate";
+import { resolve } from "node:path";
+
+const defaultMigationOptions = {
+  dryRun: false,
+  dir: resolve("infra", "migrations"),
+  direction: "up",
+  verbose: true,
+  migrationsTable: "pgmigrations",
+};
+
+async function listPendingMigrations() {
+  let dbClient;
+  try {
+    dbClient = await database.getNewClient();
+
+    const pendingMigrations = await migrationRunner({
+      ...defaultMigationOptions,
+      dbClient,
+    });
+
+    return pendingMigrations;
+  } catch (error) {
+    const serviceErrorObject = new ServiceError({
+      message: "Erro ao rodar as migrations",
+      cause: error,
+    });
+    throw serviceErrorObject;
+  } finally {
+    await dbClient?.end();
+  }
+}
+
+async function runPendingMigrations() {
+  let dbClient;
+  try {
+    dbClient = await database.getNewClient();
+
+    const migratedMigrations = await migrationRunner({
+      ...defaultMigationOptions,
+      dbClient,
+      dryRun: false,
+    });
+
+    return migratedMigrations;
+  } catch (error) {
+    const serviceErrorObject = new ServiceError({
+      message: "Erro ao rodar as migrations",
+      cause: error,
+    });
+    throw serviceErrorObject;
+  } finally {
+    await dbClient?.end();
+  }
+}
+
+const migrator = {
+  listPendingMigrations,
+  runPendingMigrations,
+};
+
+export default migrator;
