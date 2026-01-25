6a2eadca63025f90c4374fc8be2105f125ac36c6
Mateus Davi Bernart <mateusbernart14@gmail.com>
2026-01-25 16:03:53 -0300

    feat: create session by user id when correct email and password are provided, tests included

--- models/user.js
+++ models/user.js
@@ -2,6 +2,35 @@ import database from "infra/database.js";
 import { ValidationError, NotFoundError } from "infra/errors";
 import password from "../models/password.js";
 
+async function findOneByEmail(email) {
+  const userFound = await runSelectQuery(email);
+  return userFound;
+
+  async function runSelectQuery(email) {
+    const results = await database.query({
+      text: `
+      SELECT 
+        * 
+      FROM 
+        users
+      WHERE 
+        LOWER(email) = LOWER($1)
+      LIMIT 1
+      ;`,
+      values: [email],
+    });
+
+    if (results.rowCount === 0) {
+      throw new NotFoundError({
+        message: "O email informado não foi encontrado no sistema.",
+        action: "Verifique se o email está digitado corretamente.",
+      });
+    }
+
+    return results.rows[0];
+  }
+}
+
 async function findOneByUsername(username) {
   const userFound = await runSelectQuery(username);
   return userFound;
@@ -160,6 +189,7 @@ async function hashPasswordInObject(userInputValues) {
 const user = {
   create,
   findOneByUsername,
+  findOneByEmail,
   update,
 };
 
